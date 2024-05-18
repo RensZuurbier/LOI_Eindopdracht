5beda79c50589ec17a991f42332b2fad43547e4a
RensZuurbier <rensiezuurbier@hotmail.com>
2024-05-18 15:42:37 +0200

    Delete py4e directory

--- py4e/assignment_2.py
+++
@@ -1,9 +0,0 @@
-# Practice expressions
-hrs = input("Enter the hours: ")
-rate = input("Enter the rate: ")
-
-pay = float(hrs) * float(rate)
-#int_hrs = float(hrs)
-#int_rate = float(rate)
-
-print("Pay:", pay)
--- py4e/while_loop.py
+++
@@ -1,7 +0,0 @@
-n = 5
-while n > 0:
-        print(n)
-        n = n -1
-        print("Still larger")
-print(n)
-print("End of the road pall")
